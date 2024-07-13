d3616d0ee72908d17a36fe74cddb7831fdfce830
qazal <qazal.software@gmail.com>
2024-07-13 17:24:53 +0300

    run ci

--- test/external/process_replay/restore.py
+++ test/external/process_replay/restore.py
@@ -2,7 +2,7 @@ import pickle
 from tinygrad.codegen.kernel import Kernel
 from tinygrad.device import Device
 from tinygrad.engine.graph import print_tree
-from tinygrad.helpers import Context, db_connection, VERSION, ContextVar
+from tinygrad.helpers import Context, db_connection, VERSION, ContextVar, tqdm
 
 conn = db_connection()
 sha = "03c3b14cc23930eb86d55331f13b2d7135a8e836"
@@ -14,7 +14,8 @@ if __name__ == "__main__":
   conn = db_connection()
   cur = conn.cursor()
   row_count = cur.execute(f"select count(*) from '{table_name}'").fetchone()[0]
-  for offset in range(0, row_count, page_size):
+  print(row_count)
+  for offset in tqdm(range(0, row_count, page_size)):
     cur.execute(f"SELECT val FROM '{table_name}' LIMIT ? OFFSET ?", (page_size, offset))
     for row in cur.fetchall():
       ast, opts, applied_opts, name, compare_src, ctx = pickle.loads(row[0])
